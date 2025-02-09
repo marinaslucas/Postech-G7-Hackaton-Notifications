5e6ac7c14fe4c0165944ab2ef420710446ad3615
Marina Lucas <71855361+marinaslucas@users.noreply.github.com>
2025-02-08 21:08:53 -0300

    feat: separate services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,14 @@
 import { Module } from '@nestjs/common';
 import { EnvConfigModule } from './shared/infraestructure/env-config/env-config.module';
-import { UsersModule } from './users/infraestructure/users.module';
 import { DatabaseModule } from './shared/infraestructure/database/database.module';
 import { AuthModule } from './auth/infraestructure/auth.module';
-import { VideoModule } from './video/infraestructure/video.module';
 import { NotificationsModule } from './notifications/infraestructure/notification.module';
 
 @Module({
   imports: [
     EnvConfigModule,
-    UsersModule,
     DatabaseModule,
     AuthModule,
-    VideoModule,
     NotificationsModule
   ],
 })
--- src/notifications/application/usecases/send-notification.usecase.ts
+++ src/notifications/application/usecases/send-notification.usecase.ts
@@ -15,7 +15,6 @@ export class SendNotificationUseCase {
   constructor(private notificationRepository: NotificationRepository.Repository) {}
 
   async execute(input: SendNotificationInput): Promise<void> {
-    // Criando a entidade de notificação
     const notification = new NotificationEntity({
       destinatario: input.destinatario,
       titulo: input.titulo,
@@ -23,11 +22,9 @@ export class SendNotificationUseCase {
       userId: input.userId,
     });
 
-    // Salvando a notificação no repositório
     await this.notificationRepository.insert(notification);
 
     try {
-      // Chamando a API externa para enviar o e-mail
       await axios.post(this.EMAIL_API_URL, null, {
         params: {
           email: input.destinatario,
